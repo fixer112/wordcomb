4f9f2d431df45092f1ef80f1bd709daa8e4fa916
Abubakar Lawal <abula3003@gmail.com>
2020-09-16 20:44:04 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'dart:convert';
 import './listview.dart';
 import 'package:admob_flutter/admob_flutter.dart';
 
-void main() { 
+void main() {
   Admob.initialize('ca-app-pub-8524957116437815~5437189004');
   runApp(new MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.blue,
-        fontFamily: 'Montserrat',
-      ),
-      home: new Home(),
-    ));
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      primaryColor: Colors.blue,
+      fontFamily: 'Montserrat',
+    ),
+    home: new Home(),
+  ));
 }
+
 class Home extends StatefulWidget {
   @override
   HomeState createState() => HomeState();
@@ -39,42 +39,41 @@ class HomeState extends State<Home> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   AdmobInterstitial interstitialAd = AdmobInterstitial(
-  adUnitId: 'ca-app-pub-3940256099942544/1033173712',
-);
+    adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+  );
 
+  _loadJson() async {
+    String jsonCrossword = await DefaultAssetBundle.of(context)
+        .loadString('assets/json/words.json');
+    //return await DefaultAssetBundle.of(context).loadString('assets/data/crossword.json');
+    //return jsonCrossword;
+    jsonWord = jsonDecode(jsonCrossword);
+  }
 
-    _loadJson() async {
-    String jsonCrossword = await DefaultAssetBundle.of(context).loadString('assets/json/words.json');
-  //return await DefaultAssetBundle.of(context).loadString('assets/data/crossword.json');
-  //return jsonCrossword;
-  jsonWord = jsonDecode(jsonCrossword);
-}
-
-  permutation (String str, String prefix, int lengthOfPermutationString) async{
-		  if(prefix.length == lengthOfPermutationString){
-        //setState(() {
-          if (jsonWord[prefix] != null) {
-            words.add(prefix);
-          }
-          
-        //});
-        
-		  }else{
-		   for (int i = 0; i < str.length; i++) {
-		    permutation(str, prefix + str[i], lengthOfPermutationString);
-		   }
+  permutation(String str, String prefix, int lengthOfPermutationString) async {
+    if (prefix.length == lengthOfPermutationString) {
+      //setState(() {
+      if (jsonWord[prefix] != null) {
+        words.add(prefix);
       }
-       words = words.toSet().toList();
-      return words;
-     
-  } 
+
+      //});
+
+    } else {
+      for (int i = 0; i < str.length; i++) {
+        permutation(str, prefix + str[i], lengthOfPermutationString);
+      }
+    }
+    words = words.toSet().toList();
+    return words;
+  }
 
   @override
   void initState() {
     super.initState();
     _loadJson();
-    
   }
+
   @override
   void dispose() {
     wordController.dispose();
@@ -92,141 +91,139 @@ class HomeState extends State<Home> {
       ),
       //backgroundColor: Colors.blue,
       body: Container(
-          child: Column(
-        children: <Widget>[
-          
-          AdmobBanner(
-            adUnitId:testID,
-            adSize: AdmobBannerSize.BANNER,
-          ),
-          Padding(padding: EdgeInsets.all(10.0)),
-          Container(
-            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-            child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  wordLength = value.length;
-                });
-              },
-              controller: wordController,
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
-                labelText: "Type Word to Unscramble",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                    width: 3.0,
+        child: Column(
+          children: <Widget>[
+            AdmobBanner(
+              adUnitId: testID,
+              adSize: AdmobBannerSize.BANNER,
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+            Container(
+              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    wordLength = value.length;
+                  });
+                },
+                controller: wordController,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
+                  labelText: "Type Word to Unscramble",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 3.0,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Padding(padding: EdgeInsets.all(10.0)),
-          Container(
-            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-            child: Center(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.only(right: 30.0),
-                      child: Form(
-                        key: _formKey,
-                        child: TextFormField(
-                          controller: lengthController,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Cannot be empty';
-                            }
-                            if (int.parse(value) > wordLength) {
-                              return 'Cannot be greater than $wordLength';
-                            }
-                          },
-                          keyboardType: TextInputType.numberWithOptions(),
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.black,
-                          ),
-                          decoration: InputDecoration(
-                            contentPadding:
-                                const EdgeInsets.symmetric(vertical: 10.0),
-                            labelText: "Lenght",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5.0)),
-                              borderSide: BorderSide(
-                                color: Colors.blue,
-                                width: 3.0,
+            Padding(padding: EdgeInsets.all(10.0)),
+            Container(
+              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+              child: Center(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 30.0),
+                        child: Form(
+                          key: _formKey,
+                          child: TextFormField(
+                            controller: lengthController,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Cannot be empty';
+                              }
+                              if (int.parse(value) > wordLength) {
+                                return 'Cannot be greater than $wordLength';
+                              }
+                            },
+                            keyboardType: TextInputType.numberWithOptions(),
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.black,
+                            ),
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  const EdgeInsets.symmetric(vertical: 10.0),
+                              labelText: "Lenght",
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5.0)),
+                                borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 3.0,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Center(
-                    child: RaisedButton(
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: Text("Find"),
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-                          // final snackBar = SnackBar(content: Text('Loading Unscrambled Words......'));
-                          // _scaffoldKey.currentState.showSnackBar(snackBar);
-                          
-                          if( (_clicks % 5== 0 || _clicks == 2 ) && enableClick){
+                    Center(
+                      child: RaisedButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text("Find"),
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            // final snackBar = SnackBar(content: Text('Loading Unscrambled Words......'));
+                            // _scaffoldKey.currentState.showSnackBar(snackBar);
 
-                             interstitialAd.load();
-                             interstitialAd.show();
-                            
-                          }
-                          if (_clicks >= 10) {
-                            enableClick = false;
-                          }
-                          _clicks++;
+                            if ((_clicks % 5 == 0 || _clicks == 2) &&
+                                enableClick) {
+                              interstitialAd.load();
+                              interstitialAd.show();
+                            }
+                            if (_clicks >= 10) {
+                              enableClick = false;
+                            }
+                            _clicks++;
                             //print("clicking $_clicks");
 
-                          setState(() {
-                            
-                          word = wordController.text.toLowerCase();
-                        });
-                        words = [];
-                        permutation(word, "", int.parse(lengthController.text)).then((List list){
-                           setState(() {
-                           
-                          });
-                        });
-                        }
-                        
-                      },
+                            setState(() {
+                              word = wordController.text.toLowerCase();
+                            });
+                            words = [];
+                            permutation(
+                                    word, "", int.parse(lengthController.text))
+                                .then((List list) {
+                              setState(() {});
+                            });
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(padding: EdgeInsets.all(10.0)),
-          AdmobBanner(
-            adUnitId:testID,
-            adSize: AdmobBannerSize.BANNER,
-          ),
-          Center(
-            child: Text("Total Unscrambled Words : " + words.length.toString()),
-          ),
-          Expanded(
-            child:Lister(data: words),
-          ),
-          AdmobBanner(
-            adUnitId:testID,
-            adSize: AdmobBannerSize.BANNER,
-          ),
-           ],
-      )),
+            Padding(padding: EdgeInsets.all(10.0)),
+            AdmobBanner(
+              adUnitId: testID,
+              adSize: AdmobBannerSize.BANNER,
+            ),
+            Center(
+              child:
+                  Text("Total Unscrambled Words : " + words.length.toString()),
+            ),
+            Expanded(
+              child: Lister(data: words),
+            ),
+            AdmobBanner(
+              adUnitId: testID,
+              adSize: AdmobBannerSize.BANNER,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
